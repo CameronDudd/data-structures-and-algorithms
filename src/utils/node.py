6f9e1ba0334c794bc4d4f82285327274050e7cff
Cameron Dudd <110551761+CameronDudd@users.noreply.github.com>
2024-01-27 18:46:08 +0000

    feat (node): support total ordering for nodes

--- src/utils/node.py
+++ src/utils/node.py
@@ -1,10 +1,13 @@
 """Nodes"""
 
+from __future__ import annotations
 from typing import Optional, Any
 from dataclasses import dataclass
+from functools import total_ordering
 
 
 @dataclass
+@total_ordering
 class Node:
     """Node data structure"""
 
@@ -13,6 +16,16 @@ class Node:
 
     def __str__(self) -> str:
         return str(self.data)
+
+    def __eq__(self, other: Any) -> bool:
+        if isinstance(self, Node) and isinstance(other, Node):
+            return self.data == other.data
+        return False
+
+    def __lt__(self, other: Node) -> bool:
+        if isinstance(self.data, int) and isinstance(other.data, int):
+            return self.data < other.data
+        raise TypeError
 
 
 @dataclass
